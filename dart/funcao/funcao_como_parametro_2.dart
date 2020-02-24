1acaaf031eba08dd154a5ea36f8a36a00250c052
Wellington Lopes <wellington.lopes.tw@gmail.com>
2020-02-24 14:44:03 -0400

    dart funçoes

---
+++ dart/funcao/funcao_como_parametro_2.dart
@@ -0,0 +1,16 @@
+int executarPor(int qtde, String Function(String) fn, String valor) {
+  String textoCompleto = '';
+  for (int i = 0; i < qtde; i++) {
+    textoCompleto += fn(valor);
+  }
+  return textoCompleto.length;
+}
+
+main() {
+  var meuPrint = (String valor) {
+    print(valor);
+    return valor;
+  };
+  int tamanho = executarPor(10, meuPrint, "Muito legal");
+  print("O tamanho da String é $tamanho");
+}
